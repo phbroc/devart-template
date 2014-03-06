3d4b2ab3ccdd4f9d45ce82473563afc5d8326224
Philippe Brouard <philippe.brouard@laposte.net>
2014-03-06 22:44:54 +0100

    Commit number 4 adding keyboard control
    Keyboard control is better managed in a class

--- project_code/beziercurvesbigcircus/web/beziercurvesbigcircus.dart
+++ project_code/beziercurvesbigcircus/web/beziercurvesbigcircus.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'dart:html';
 import 'dart:math';
+import 'dart:core';
+import 'package:csslib/parser.dart';
 
 const MAX_W = 800;
 const MAX_H = 600;
@@ -9,18 +11,16 @@ const CENTER_Y = 300;
 const SCALE_X = 250;
 const SCALE_Y = 150;
 
-var keyPressedCode = 0;
 
 BezierCanvas bezierCanvas;
+KeyboardController keyboardController;
 
 void main() {
-  document.onKeyDown.listen(keydown);
-  document.onKeyUp.listen(keyup);
-  
-  
+  keyboardController = new KeyboardController();
   CanvasElement canvas = querySelector("#canvas");
   bezierCanvas = new BezierCanvas(canvas);
   scheduleMicrotask(bezierCanvas.start);
+  bindControls();
 }
 
 
@@ -35,29 +35,101 @@ void showFps(num fps) {
   notes.text = "${fpsAverage.round()} fps";
 }
 
+void bindControls() {
+  
+  window.on['lineWidthUp'].listen((e) {
+    if (e.detail) bezierCanvas.dLineWidth = 1;
+    else bezierCanvas.dLineWidth = 0;
+    });
+  
+  window.on['lineWidthDown'].listen((e) {
+      if (e.detail) bezierCanvas.dLineWidth = -1;
+      else bezierCanvas.dLineWidth = 0;
+      });
+  
+  window.on['stpParamUp'].listen((e) {
+      if (e.detail) bezierCanvas.dStpParam = 0.05;
+      else bezierCanvas.dStpParam = 0.0;
+      });
+    
+  window.on['stpParamDown'].listen((e) {
+      if (e.detail) bezierCanvas.dStpParam = -0.05;
+      else bezierCanvas.dStpParam = 0.0;
+      });
+  
+  window.on['colorHueUp'].listen((e) {
+      if (e.detail) bezierCanvas.dC1h = 1;
+      else bezierCanvas.dC1h = 0;
+      });
+    
+  window.on['colorHueDown'].listen((e) {
+      if (e.detail) bezierCanvas.dC1h = -1;
+      else bezierCanvas.dC1h = 0;
+      });
+  
+  window.on['colorSatUp'].listen((e) {
+        if (e.detail) bezierCanvas.dC1s = 1;
+        else bezierCanvas.dC1s = 0;
+        });
+      
+  window.on['colorSatDown'].listen((e) {
+      if (e.detail) bezierCanvas.dC1s = -1;
+      else bezierCanvas.dC1s = 0;
+      });
+  
+  window.on['colorLumUp'].listen((e) {
+          if (e.detail) bezierCanvas.dC1l = 1;
+          else bezierCanvas.dC1l = 0;
+          });
+        
+  window.on['colorLumDown'].listen((e) {
+      if (e.detail) bezierCanvas.dC1l = -1;
+      else bezierCanvas.dC1l = 0;
+      });
+  
+}
+
 class BezierCanvas
 {
   CanvasElement canvas;
   CanvasRenderingContext2D ctx2d;
-  
+  ImageElement hslpicker;
+  CanvasElement hsllum;
+  ImageElement lumpicker;
 
-  var aParam = 1.1;
-  var bParam = 0.1;
-  var stpParam = 1.0;
-  var bzrParam = 0.65;
+  double aParam = 1.1;
+  double bParam = 0.1;
+  double stpParam = 1.0;
+  double dStpParam = 0.0;
+  double bzrParam = 0.65;
   
-  var lineWidth = 20;
+  int c1h = 0;
+  int dC1h = 0;
+  int c1s = 100;
+  int dC1s = 0;
+  int c1l = 50;
+  int dC1l = 0;
+  double c1a = 1.0;
+  double dC1a = 0.0;
+  
+  int lineWidth = 20;
+  int dLineWidth = 0;
   
   num renderTime;
   
   BezierCanvas(this.canvas) {
     ctx2d = canvas.context2D;
+    hslpicker = querySelector("#hslpicker") as ImageElement;
+    hsllum = querySelector("#hsllum") as CanvasElement;
+    lumpicker = querySelector("#lumpicker") as ImageElement;
   }
+  
   
   void start() {
     requestRedraw();
   }
   
+
   
   void draw(num _) {
     double xa, ya, xm, ym, xb, yb, xcpa, ycpa, xcpb, ycpb, t;
@@ -69,16 +141,10 @@ class BezierCanvas
     if (renderTime != null) showFps(1000 / (time - renderTime));
     renderTime = time;
     
-    notes2.text = "key pressed : " + keyPressedCode.toString();
     
-    if (keyPressedCode == 65) lineWidth++;
-    else if ((keyPressedCode == 90) && (lineWidth > 1)) lineWidth--;
-    else if (keyPressedCode == 81) aParam += 0.001;
-    else if (keyPressedCode == 83) aParam -= 0.001;
-    else if (keyPressedCode == 87) bParam += 0.001;
-    else if (keyPressedCode == 88) bParam -= 0.001;
-    else if (keyPressedCode == 69) stpParam += 0.1;
-    else if ((keyPressedCode == 82) && (stpParam > 0.2)) stpParam -= 0.1;
+ 
+    updateVariables();
+    
     
     ctx2d.clearRect(0, 0, MAX_W, MAX_H);
     ctx2d.setFillColorRgb(240, 240, 240, 1);
@@ -104,8 +170,8 @@ class BezierCanvas
       xcpb = xb - (xb - ((xa+xb)/2 + 2.2*(xm-(xa+xb)/2)))*bzrParam;
       ycpb = yb - (yb - ((ya+yb)/2 + 2.2*(ym-(ya+yb)/2)))*bzrParam;
       
-      if (i % 2 == 0) ctx2d.setStrokeColorRgb(90, 200, 220, 1);
-      else ctx2d.setStrokeColorRgb(220, 90, 200, 1);
+      if (i % 2 == 0) ctx2d.setStrokeColorHsl(c1h, c1s, c1l, c1a);
+      else ctx2d.setStrokeColorRgb(170, 170, 170, 1);
       
       ctx2d.beginPath();
       ctx2d.moveTo(xScreen(xa), yScreen(ya));
@@ -120,6 +186,48 @@ class BezierCanvas
   void requestRedraw() {
       window.requestAnimationFrame(draw);
   }
+  
+  
+  
+  
+  
+  
+  
+  void updateVariables() {
+    lineWidth = max(1, min(255, lineWidth + dLineWidth));
+    stpParam = max(0.04, stpParam + dStpParam);
+    
+    c1h = max(0, min(360, c1h + dC1h));
+    c1s = max(0, min(100, c1s + dC1s));
+    c1l = max(0, min(100, c1l + dC1l));
+    
+    updateColorPicker();
+    
+    
+    
+  }
+  
+  
+  void updateColorPicker() {
+    var hsllumctx = hsllum.context2D;
+    hslpicker.style.left = (c1h/2).round().toString() + "px";
+    hslpicker.style.top = (100-c1s).round().toString() + "px";
+    
+    hsllumctx.clearRect(0, 0, 30, 100);
+    var gradient = hsllumctx.createLinearGradient(0, 0, 0, 150);
+    var color = new Color.createHsla(c1h, c1s, 50, 1);
+    gradient.addColorStop(0, "#fff");
+    gradient.addColorStop(0.5, color.rgba.cssExpression);
+    gradient.addColorStop(1, "#000");
+    hsllumctx.fillStyle = gradient;
+    hsllumctx.fillRect(0, 0, 300, 150);
+    
+    lumpicker.style.left = "205px";
+    lumpicker.style.top = (100-c1l).round().toString() + "px";
+  }
+  
+  
+  
   
   
   double xFormula(double t) {
@@ -137,12 +245,48 @@ class BezierCanvas
   double yScreen(double y) {
     return y*SCALE_Y + CENTER_Y;
   }
+  
 }
 
-void keydown(KeyboardEvent event) {
-    keyPressedCode = event.keyCode;
+class KeyboardController
+{
+  Map keyCodesNormal; 
+  
+  KeyboardController() {
+      keyCodesNormal = new Map();
+      for (var i = 0; i < 300; i++) {
+        keyCodesNormal[i] = "nothing";
+      }
+      
+      initialiseKeyCodesBinding();
+      
+      document.onKeyDown.listen(keydown);
+      document.onKeyUp.listen(keyup);
+  }
+  
+  void initialiseKeyCodesBinding()
+  {
+    keyCodesNormal[65] = "lineWidthUp";
+    keyCodesNormal[81] = "lineWidthDown";
+    keyCodesNormal[90] = "stpParamUp";
+    keyCodesNormal[83] = "stpParamDown";
+    
+    keyCodesNormal[85] = "colorHueUp";
+    keyCodesNormal[74] = "colorHueDown";
+    keyCodesNormal[73] = "colorSatUp";
+    keyCodesNormal[75] = "colorSatDown";
+    keyCodesNormal[79] = "colorLumUp";
+    keyCodesNormal[76] = "colorLumDown";
+  }
+  
+  void keydown(KeyboardEvent event) {
+      notes2.text = "key pressed : " + event.keyCode.toString();
+      window.dispatchEvent(new CustomEvent(keyCodesNormal[event.keyCode], detail:true));
+  }
+
+  void keyup(KeyboardEvent event) {
+      notes2.text = "key pressed : ";
+      window.dispatchEvent(new CustomEvent(keyCodesNormal[event.keyCode], detail:false));
+  }
 }
 
-void keyup(KeyboardEvent event) {
-    keyPressedCode = 0;
-}
---
+++ project_posts/2014-03-06-Adding-Keyboard-controller.md
@@ -0,0 +1,63 @@
+# ADDING KEYBOARD CONTROLLER
+I have in mind to hack some keyboard interface in order to build my final panel for user control, with custom joysticks and buttons. At first I have to make sure that's possible to handle the parametric equation with keyboard events. I want to add many controls, and therefore my source code must stay clear... I thought of adding a new class for keyboard events. I built this one :
+
+```
+class KeyboardController
+{
+  Map keyCodesNormal; 
+  
+  KeyboardController() {
+      keyCodesNormal = new Map();
+      for (var i = 0; i < 300; i++) {
+        keyCodesNormal[i] = "nothing";
+      }
+      
+      initialiseKeyCodesBinding();
+      
+      document.onKeyDown.listen(keydown);
+      document.onKeyUp.listen(keyup);
+  }
+  
+  void initialiseKeyCodesBinding() {
+    keyCodesNormal[65] = "lineWidthUp";
+    keyCodesNormal[81] = "lineWidthDown";
+    keyCodesNormal[90] = "stpParamUp";
+    keyCodesNormal[83] = "stpParamDown";
+    
+    keyCodesNormal[85] = "colorHueUp";
+    keyCodesNormal[74] = "colorHueDown";
+    keyCodesNormal[73] = "colorSatUp";
+    keyCodesNormal[75] = "colorSatDown";
+    keyCodesNormal[79] = "colorLumUp";
+    keyCodesNormal[76] = "colorLumDown";
+  }
+  
+  void keydown(KeyboardEvent event) {
+      window.dispatchEvent(new CustomEvent(keyCodesNormal[event.keyCode], detail:true));
+  }
+
+  void keyup(KeyboardEvent event) {
+      window.dispatchEvent(new CustomEvent(keyCodesNormal[event.keyCode], detail:false));
+  }
+}
+```
+
+For example, when you press A letter in keyboard, it brings an event with keyCode 65. Then a new event is dispatched, with a clearer message = "lineWidthUp". It means that I want to increase the width of lines. Next, in the window the code has to listen at this message :
+
+```
+window.on['lineWidthUp'].listen((e) {
+    if (e.detail) bezierCanvas.dLineWidth = 1;
+    else bezierCanvas.dLineWidth = 0;
+    });
+``
+
+dLineWidth is the growth of line width every frames.
+
+I have completed the source code with a colorPicker driven by keyboard.
+
+This is what I've got today :
+
+![Curve 03](../project_images/curve03.jpg?raw=true "Curve 03")
+
+
+That's all for my post number 4. :->
